bb3fa7590316f2899cab877a892de7a7a7eab07e
Musavir <88766824+Musavir-Ali@users.noreply.github.com>
2024-03-30 04:27:19 +0500

    Update

--- lib/Subscriptions.dart
+++ lib/Subscriptions.dart
@@ -1,10 +1,5 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:circular_seek_bar/circular_seek_bar.dart';
-import 'package:curved_navigation_bar/curved_navigation_bar.dart';
-import 'package:flutter/widgets.dart';
-import 'package:get/get_connect/http/src/utils/utils.dart';
-import 'package:trackizer/Tab_Sub.dart';
 
 class Subs extends StatefulWidget {
   const Subs({Key? key}) : super(key: key);
@@ -14,26 +9,13 @@ class Subs extends StatefulWidget {
 }
 
 class _SubsState extends State<Subs> {
-  final ValueNotifier<double> _valueNotifier = ValueNotifier(0);
   double _progress = 76;
-  double _startAngle = 45;
-  double _sweepAngle = 270;
-  double _dashWidth = 0;
-  double _dashGap = 0;
-  double _barWidth = 8.0;
-  bool _useGradient = true;
-  bool _rounded = true;
-  bool _animation = true;
-  bool _thumbVisible = true;
-  bool _interactive = true;
-
-  int _page = 0;
-  GlobalKey<CurvedNavigationBarState> _bottomNavigationKey = GlobalKey();
 
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
       length: 2,
+      initialIndex: 1,
       child: Scaffold(
         body: Column(
           children: [
@@ -44,38 +26,39 @@ class _SubsState extends State<Subs> {
                 child: Stack(
                   children: [
                     Center(
-                        child: CircularSeekBar(
-                      width: double.infinity,
-                      trackColor: Colors.grey,
-                      outerThumbColor: const Color(0xFF83839C),
-                      height: 240,
-                      progress: _progress,
-                      barWidth: 15,
-                      startAngle: 45,
-                      sweepAngle: 270,
-                      strokeCap: StrokeCap.round,
-                      progressGradientColors: const [
-                        Colors.green,
-                        Colors.deepOrange,
-                      ],
-                      dashWidth: 1,
-                      dashGap: 2,
-                      animation: true,
-                      child: Center(
-                        child: Column(
-                          children: [
-                            SizedBox(
+                      child: CircularSeekBar(
+                        width: double.infinity,
+                        trackColor: Colors.grey,
+                        outerThumbColor: const Color(0xFF83839C),
+                        height: 240,
+                        progress: _progress,
+                        barWidth: 15,
+                        startAngle: 45,
+                        sweepAngle: 270,
+                        strokeCap: StrokeCap.round,
+                        progressGradientColors: const [
+                          Colors.green,
+                          Colors.deepOrange,
+                        ],
+                        dashWidth: 1,
+                        dashGap: 2,
+                        animation: true,
+                        child: Center(
+                          child: Column(
+                            children: [
+                              SizedBox(
                                 height:
-                                    MediaQuery.of(context).size.height * 0.1),
-                            Image.asset(
-                              "assets/images/logo.png",
-                              width: 100,
-                            ),
-                            SizedBox(
+                                    MediaQuery.of(context).size.height * 0.1,
+                              ),
+                              Image.asset(
+                                "assets/images/logo.png",
+                                width: 100,
+                              ),
+                              SizedBox(
                                 height:
-                                    MediaQuery.of(context).size.height * 0.03),
-                            const SizedBox(
-                              child: Text(
+                                    MediaQuery.of(context).size.height * 0.03,
+                              ),
+                              const Text(
                                 "\$118",
                                 style: TextStyle(
                                   fontSize: 32,
@@ -83,57 +66,57 @@ class _SubsState extends State<Subs> {
                                   fontWeight: FontWeight.bold,
                                 ),
                               ),
-                            ),
-                            SizedBox(
+                              SizedBox(
                                 height:
-                                    MediaQuery.of(context).size.height * 0.01),
-                            const SizedBox(
-                              child: Text(
+                                    MediaQuery.of(context).size.height * 0.01,
+                              ),
+                              const Text(
                                 "This month bills",
                                 style: TextStyle(
                                   fontSize: 12,
                                   color: const Color(0xFF83839C),
                                 ),
                               ),
-                            ),
-                            SizedBox(
+                              SizedBox(
                                 height:
-                                    MediaQuery.of(context).size.height * 0.01),
-                            SizedBox(
-                                child: ElevatedButton(
-                              onPressed: () {},
-                              child: Text(
-                                "Pay Now",
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 12,
+                                    MediaQuery.of(context).size.height * 0.01,
+                              ),
+                              ElevatedButton(
+                                onPressed: () {},
+                                child: Text(
+                                  "Pay Now",
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 12,
+                                  ),
+                                ),
+                                style: ElevatedButton.styleFrom(
+                                  backgroundColor: const Color(0xFF83839C),
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(20),
+                                  ),
                                 ),
                               ),
-                              style: ElevatedButton.styleFrom(
-                                backgroundColor: const Color(0xFF83839C),
-                                shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(20),
-                                ),
-                              ),
-                            )),
-                          ],
-                        ),
-                      ),
-                    )),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        IconButton(
-                          onPressed: () {},
-                          icon: Icon(
-                            Icons.settings,
-                            color: Colors.white,
+                            ],
                           ),
                         ),
-                      ],
+                      ),
                     ),
-                    SizedBox(
-                      height: MediaQuery.of(context).size.height * 0.49,
+                    Positioned(
+                      top: 0,
+                      right: 0,
+                      child: IconButton(
+                        onPressed: () {},
+                        icon: Icon(
+                          Icons.settings,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      bottom: 0,
+                      left: 0,
+                      right: 0,
                       child: Row(
                         mainAxisAlignment: MainAxisAlignment.spaceAround,
                         crossAxisAlignment: CrossAxisAlignment.end,
@@ -245,7 +228,7 @@ class _SubsState extends State<Subs> {
                       padding: const EdgeInsets.symmetric(horizontal: 20),
                       child: Column(
                         children: [
-                          const SizedBox(
+                          SizedBox(
                             height: 5,
                           ),
                           Container(
@@ -254,18 +237,56 @@ class _SubsState extends State<Subs> {
                               color: Colors.black,
                               borderRadius: BorderRadius.circular(10),
                             ),
-                            child: TabBar(
-                                indicatorColor: Colors.white,
-                                labelColor: Colors.white,
-                                dividerColor: Colors.transparent,
-                                tabs: [
-                                  Tab(
-                                    text: "Subscriptions",
+                            child: const TabBar(
+                              isScrollable: true,
+                              indicatorColor: Colors.white,
+                              labelColor: Colors.white,
+                              dividerColor: Colors.transparent,
+                              tabs: [
+                                Tab(
+                                  child: Text("Subscriptions"),
+                                ),
+                                Tab(
+                                  child: Text("Upcoming Bills"),
+                                ),
+                              ],
+                            ),
+                          ),
+                          Expanded(
+                            child: TabBarView(
+                              children: [
+                                ListView.builder(
+                                  scrollDirection: Axis.vertical,
+                                  physics: const BouncingScrollPhysics(),
+                                  itemCount: 2,
+                                  itemBuilder: (context, index) {
+                                    return ListTile(
+                                      title: Text("data"),
+                                    );
+                                  },
+                                ),
+                                SingleChildScrollView(
+                                  scrollDirection: Axis.horizontal,
+                                  physics: const BouncingScrollPhysics(),
+                                  child: Column(
+                                    children: List.generate(2, (index) {
+                                      return SizedBox(
+                                        width:
+                                            MediaQuery.of(context).size.width *
+                                                0.8,
+                                        child: Card(
+                                          color:
+                                              Color.fromARGB(255, 31, 30, 30),
+                                          child: ListTile(
+                                            title: Text("data"),
+                                          ),
+                                        ),
+                                      );
+                                    }),
                                   ),
-                                  Tab(
-                                    text: "Upcomming Bills",
-                                  ),
-                                ]),
+                                ),
+                              ],
+                            ),
                           ),
                         ],
                       ),
